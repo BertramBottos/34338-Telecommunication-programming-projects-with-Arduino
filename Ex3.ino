f17fcb391dbcc594d5ca2590f9c25e039cf21ef4
Bertram Bottos <96624038+BertramBottos@users.noreply.github.com>
2023-01-04 09:37:47 +0100

    Update Ex3.ino

--- Ex3.ino
+++ Ex3.ino
@@ -1,13 +1,22 @@
-int incomingByte = 0;
+int button = 7;
+int greenLED = 2;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(greenLED, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+
 }
 
+
 void loop() {
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    Serial.print("I recceived: ");
-    Serial.println(incomingByte, DEC);
+  Serial.println(digitalRead(button));
+
+  if (!digitalRead(button)) {
+    digitalWrite(greenLED, !digitalRead(greenLED));
+    while (!digitalRead(button)) {
+    
+    }
   }
+
 }
